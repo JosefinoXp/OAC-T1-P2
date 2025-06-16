15cff1eaef54de3725843cd113fa4bd71ed69961
JosefinoXp <joselucasxp345@gmail.com>
2025-06-16 00:38:51 -0300

    Decisão Final CPUbound (Numeros Primos)

--- CPUbound.java
+++ CPUbound.java
@@ -1,24 +1,46 @@
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class CPUBound {
-    static long TorreDeHanoi(long n) {
-        if (n == 0) return 0;
-        return 1 + 2 * TorreDeHanoi(n - 1);
+    public static boolean EhPrimo(long numeroAtual) {
+        if (numeroAtual <= 1) return false;
+        for (long i = 2; i <= Math.sqrt(numeroAtual); i++) {
+            if (numeroAtual % i == 0) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        ThreadMXBean mx = ManagementFactory.getThreadMXBean();
 
-        long ini = mx.getCurrentThreadCpuTime();
-        long moves = TorreDeHanoi(N);
-        long cpuNs = mx.getCurrentThreadCpuTime() - ini;
+        System.out.println("ATENÇÃO: NÃO ACEITAMOS NÚMEROS A PARTIR DE UM SEXTILIAO");
+        System.out.println("Digite o maior valor que vai querer verificar: ");
+        
+        ThreadMXBean medidor = ManagementFactory.getThreadMXBean();
+        Scanner entrada = new Scanner(System.in);
+        
+        long maiorNumero = entrada.nextLong();
 
-        System.out.println("Movimentos: " + moves);
-        System.out.println("Duracao em milisegundos: " + TimeUnit.NANOSECONDS.toMillis(cpuNs));
-        System.out.println("Duracao em segundos: " + TimeUnit.NANOSECONDS.toSeconds(cpuNs));
+        entrada.close();
+
+        System.out.println("Contando...");
+
+        //tempo em nanosegundos, conta depois do IO (Pois estamos so contando o CPU-bound)
+        long inicio = medidor.getCurrentThreadCpuTime();
+        
+        long contagemPrimos = 0;
+        for (long i = 2; i < maiorNumero; i++) {
+            if (EhPrimo(i)) {
+                contagemPrimos++;
+            }
+        }
+        
+        long tempoCPU = medidor.getCurrentThreadCpuTime() - inicio;
+        
+        System.out.println("Quantidade de Numeros Primos achados: " + contagemPrimos);
+        System.out.println("Quantidade de Nanosegundos da CPU: " + tempoCPU);
+        System.out.println("Quantidade de Milisegundos da CPU: " + TimeUnit.NANOSECONDS.toMillis(tempoCPU));
+        System.out.println("Quantidade de Segundos da CPU: " + TimeUnit.NANOSECONDS.toSeconds(tempoCPU));
     }
-    
 }
